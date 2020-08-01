776d13a2c5ba5b59b5c1c42a8f46fa1c7863ab14
Linda Paiste <lindapaiste@gmail.com>
2020-08-01 12:58:51 -0500

    finished version with font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { render } from "react-dom";
-import "typeface-press-start-2p";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
